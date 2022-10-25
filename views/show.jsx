f6581674dcde6f363df48b798d71559778ab129b
Benjamin Pope <buddypope1234@gmail.com>
2022-10-24 19:39:01 -0500

    delete route completed

--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show ({bread}) {
+function Show ({bread, index}) {
     // Confirm we are getting our bread data in the terminal.
     // console.log(bread.name)
       return (
@@ -15,10 +15,15 @@ function Show ({bread}) {
                 ? <span> does </span>
                 : <span> does NOT </span>
             }
-            have gluten.
+            has gluten.
           </p>
           <img src={bread.image} alt={bread.name} />
           <li><a href="/breads">Go home</a></li>
+
+          <form action={`/breads/${index}?_method=DELETE`} method="POST">
+            <input type='submit' value="DELETE"/>
+          </form>
+
         </Default>
       )
   }
